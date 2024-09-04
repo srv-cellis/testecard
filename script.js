ccda639344e9271d1f2a74e8255f6bb7f797ffea
srv-cellis <samuelvascon001@gmail.com>
2024-09-04 16:41:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function flipCard(card) {
+    // verifica se esta virad
+    if (card.classList.contains('flipped')) {
+        return;
+    }
+    
+    //aq a carta vira
+    card.classList.add('flipped');
+    
+
+    const cardBack = card.querySelector('.card-back');
+    
+
+    if (card.querySelector('.card-front').textContent === '?') {
+        if (cardBack && card.querySelector('.card-back').parentNode.parentNode.textContent.includes('Carta 1')) {
+            cardBack.textContent = "Parabéns, você ganhou + 1 hora de trabalho!";
+        } else if (cardBack && card.querySelector('.card-back').parentNode.parentNode.textContent.includes('Carta 2')) {
+            cardBack.textContent = "Parabéns, você ganhou - 2 horas de trabalho!";
+        } else if (cardBack && card.querySelector('.card-back').parentNode.parentNode.textContent.includes('Carta 3')) {
+            cardBack.textContent = "Parabéns, você me deve uma mamada!";
+        }
+    }
+}
